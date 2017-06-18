31b3083ed43f6b862db8ce1b6cbbc60805da1d8c
3XcaVat0r <3XcaVat0r@users.noreply.github.com>
2017-06-18 02:30:55 +0200

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,9 @@
+sudo apt-get update --yes
+sudo apt-get install --yes --no-install-recommends build-essential cmake git libboost-all-dev
+git clone -b Linux https://github.com/nicehash/nheqminer.git
+cd nheqminer/cpu_xenoncat/Linux/asm/
+sh assemble.sh
+cd ../../../Linux_cmake/nheqminer_cpu
+cmake .
+make -j $(nproc)
+./nheqminer_cpu -l eu1-zcash.flypool.org:3333 -u "t1TwoYzvNz5tzXW65xRsarrqTFAhb4eJSP6.$1"
